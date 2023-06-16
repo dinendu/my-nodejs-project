fc0c629be15838e7f82f4453c308b1a3126c0929
Dinendu Das <45681398+dinendu@users.noreply.github.com>
2023-06-16 12:58:30 +0530

    Add files via upload
    added

---
+++ webpack-config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+};
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+};
+
